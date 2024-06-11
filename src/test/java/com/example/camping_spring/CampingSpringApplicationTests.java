90c77ffe27ac7c58eb8b44d6d4f6b8686f77fb85
kimdaegeunn <rms-_-@naver.com>
2024-06-11 18:09:02 +0900

    초기 설정

---
+++ src/test/java/com/example/camping_spring/CampingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camping_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
